ed423af136be89ba7b660368ba4744ffae37ca5e
MisterB-974 <canouextasy@gmail.com>
2025-09-28 12:02:55 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// All your JS from the previous message goes here!
+const vocabularyCards = [
+    { front: "😍 LOVE", back: "J'adore ! (+ gérondif/nom)\n'I love dancing' 💃", hint: "Sentiment très fort et positif" },
+    { front: "😊 LIKE", back: "J'aime (+ gérondif/nom)\n'I like reading' 📚", hint: "Sentiment positif modéré" },
+    { front: "😐 DISLIKE", back: "Je n'aime pas (+ gérondif/nom)\n'I dislike waiting' ⏰", hint: "Sentiment négatif modéré" },
+    { front: "😤 HATE", back: "Je déteste (+ gérondif/nom)\n'I hate cleaning' 🧹", hint: "Sentiment très négatif" },
+    { front: "🤩 BE CRAZY ABOUT", back: "Être fou/folle de (+ gérondif/nom)\n'I'm crazy about football' ⚽", hint: "Passion extrême" },
+    { front: "😌 ENJOY", back: "Apprécier (+ gérondif seulement !)\n'I enjoy cooking' 👨‍🍳", hint: "Toujours suivi du gérondif" },
+    { front: "🧐 BE INTERESTED IN", back: "S'intéresser à (+ gérondif/nom)\n'I'm interested in science' 🔬", hint: "Curiosité intellectuelle" },
+    { front: "🥰 BE FOND OF", back: "Avoir de l'affection pour (+ gérondif/nom)\n'I'm fond of cats' 🐱", hint: "Affection tendre" },
+    { front: "😰 CAN'T BEAR", back: "Ne pas supporter (+ gérondif/infinitif)\n'I can't bear waiting' ⏳", hint: "Intolérance forte" },
+    { front: "😱 DREAD", back: "Redouter (+ gérondif/nom)\n'I dread exams' 📝", hint: "Peur anticipée" }
+];
+
+// ...rest of your JS (copy everything from your previous 'app.js' reply)...
